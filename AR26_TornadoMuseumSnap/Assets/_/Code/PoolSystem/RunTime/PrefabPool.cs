0e416140b4919447bc9dc58a481d980de5bd3ef0
Hakeron <richardyerna@gmail.com>
2026-05-06 15:44:24 +0200

    Base of PoolSystem & Cube Prefab

--- AR26_TornadoMuseumSnap/Assets/_/Code/PoolSystem/RunTime/PrefabPool.cs
+++ AR26_TornadoMuseumSnap/Assets/_/Code/PoolSystem/RunTime/PrefabPool.cs
@@ -1,21 +1,28 @@
-using System;
 using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 
 namespace PoolSystem_RunTime
 {
-    public class PrefabSpawn : MonoBehaviour
+    public class PrefabPool : MonoBehaviour
     {
         #region Publics
-        
-
-        public List<GameObject> m_artefactsPrefab;
         
 
         #endregion
 
         #region Main Methods
+
+        public GameObject GetArtefact()
+        {
+            for (int i = 0; i < _gameObjectsSpawned.Count; i++)
+            {
+                if (!_gameObjectsSpawned[i].activeInHierarchy)
+                {
+                    return _gameObjectsSpawned[i];
+                }
+            }
+            return SpawnRandomPrefab();
+        }
         
         #endregion
 
@@ -27,21 +34,37 @@ namespace PoolSystem_RunTime
         }
 
         #endregion
+        
         #region Utils
+
+
+        private GameObject SpawnRandomPrefab()
+        {
+            int random =  Random.Range(0, _artefactsPrefab.Count);
+            
+            GameObject obj = Instantiate(_artefactsPrefab[random],transform);
+            obj.SetActive(false);
+            _gameObjectsSpawned.Add(obj);
+            return obj;
+        }
 
         private void SpawnPrefab()
         {
-            for (int i = 0; i < m_artefactsPrefab.Count; i++)
+            for (int i = 0; i < _artefactsPrefab.Count; i++)
             {
-                GameObject obj = Instantiate(m_artefactsPrefab[i]);
+                GameObject obj = Instantiate(_artefactsPrefab[i],transform);
                 obj.SetActive(false);
+                _gameObjectsSpawned.Add(obj);
             }
         }
         
         #endregion
 
         #region Privates
-
+        
+        [SerializeField]private List<GameObject> _artefactsPrefab;
+        private List<GameObject> _gameObjectsSpawned = new List<GameObject>();
+        
         #endregion
     }
 }
