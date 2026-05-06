0c6588bca6f0d2943ddf044489793993d2ab01bc
Daniele <s_13980_devxr@PEDAGOGIQUE.LAN>
2026-05-06 14:15:23 +0200

    Import Package Spline Unity

---
+++ AR26_TornadoMuseumSnap/Assets/_/Code/Artifact/Runtime/ArtifactBehaviour.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ArtifactBehaviour : MonoBehaviour
+{
+    #region  Publics
+    
+    #endregion
+    
+    #region Unity API
+    
+    #endregion
+    
+    #region Main Methods
+    
+    #endregion
+    
+    #region Utils
+    
+    #endregion
+    
+    #region Privates
+    
+    #endregion
+    
+    
+}
